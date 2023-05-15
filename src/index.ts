85e5e5cbad8833cf8b5b8da5c09cf5d1581ba9ae
Felipe-Freitasleal <felipeleal094@gmail.com>
2023-05-14 22:18:15 -0300

    feat: conexão db MySQL; endpoint GET products

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,35 @@ app.listen(3003, () => {
   console.log(`Servidor rodando na porta ${3003}`);
 });
 
+// (async () => {
+//   const db = require("./db");
+//   const produtos = await db.selectProducts();
+//   console.log(produtos);
+// })();
+
 app.get("/ping", async (req: Request, res: Response) => {
   try {
     res.status(200).send({ message: "Pong!" });
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
+    }
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
+
+app.get("/products", async (req: Request, res: Response) => {
+  try {
+    const db = require("./db");
+    const produtos = await db.selectProducts();
+    res.status(200).send({ produtos: produtos, message: "Lista de Produtos" });
   } catch (error) {
     console.log(error);
 
